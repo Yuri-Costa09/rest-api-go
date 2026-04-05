ace584759561c89ad185abaef94908625fa19ae9
Yuri-Costa09 <yurircostawork@gmail.com>
2026-04-05 19:15:16 -0300

    refactor: move validation logic to Product struct method

--- internal/model/product.go
+++ internal/model/product.go
@@ -19,17 +19,34 @@ var (
 )
 
 func NewProduct(price float32, name string, description *string) (*Product, error) {
-	if price <= 0 {
-		return nil, ErrInvalidPrice
-	}
-	if len(name) < 3 {
-		return nil, ErrInvalidName
-	}
+	// if price <= 0 {
+	// 	return nil, ErrInvalidPrice
+	// }
+	// if len(name) < 3 {
+	// 	return nil, ErrInvalidName
+	// }
 
-	return &Product{
+	p := &Product{
 		ID:          uuid.NewString(),
 		Name:        name,
 		Price:       price,
 		Description: description,
-	}, nil
+	}
+
+	if err := p.validate(); err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+func (p *Product) validate() error {
+	if p.Price <= 0 {
+		return ErrInvalidPrice
+	}
+	if len(p.Name) < 3 {
+		return ErrInvalidName
+	}
+
+	return nil
 }
